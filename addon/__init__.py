de54b849ad067dee199f8a39b65ccaf53eaaf4e3
Nymic <named.blade@gmail.com>
2025-11-07 00:26:28 +0000

    blend

---
+++ addon/__init__.py
@@ -0,0 +1,9 @@
+bl_info = {
+    "name": "dsts_formats",
+    "blender": (4, 4, 3),
+    "category": "Object",
+}
+def register():
+    print("Hello World")
+def unregister():
+    print("Goodbye World")
