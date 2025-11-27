fda4150fb3dced4f8a234e8a62a843038c6e9d60
Nymic <named.blade@gmail.com>
2025-11-27 00:21:22 +0000

    clut initialization

--- src/geom/binary/clut.hpp
+++ src/geom/binary/clut.hpp
@@ -14,6 +14,12 @@ namespace dsts::geom::binary
         uint8_t unk1[0x10];
         ColorData data[0x100];
         uint8_t unk2[0xc];
+
+        constexpr Clut() : data{} {
+            for (int i = 0; i < 0x100; ++i) {
+                data[i] = { (uint8_t)i, (uint8_t)i, (uint8_t)i, (uint8_t)i };
+            }
+        }
     };
 
     #pragma pack(pop)
