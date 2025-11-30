70a2e1021d4c5e549ab70199d57b6fb1e4ccc2ec
Nymic <named.blade@gmail.com>
2025-11-30 09:39:52 +0000

    writing export skeleton

--- addon/__init__.py
+++ addon/__init__.py
@@ -1,5 +1,5 @@
 import bpy
-from bpy_extras.io_utils import ImportHelper
+from bpy_extras.io_utils import ImportHelper, ExportHelper
 from bpy.props import StringProperty
 from bpy.types import Operator
 
@@ -31,16 +31,54 @@ class MY_OT_dsts_geom_import_operator(Operator, ImportHelper):
         imported_collection = import_geom(context, filepath)
 
         return {'FINISHED'}
+    
+class MY_OT_dsts_geom_export_operator(Operator, ExportHelper):
+    bl_idname = "export_scene.dsts_geom_export"
+    bl_label = "DSTS .geom export"
+
+    # Filter file extensions (optional)
+    filename_ext = ".geom"
+    filter_glob: StringProperty(
+        default="*.geom",
+        options={'HIDDEN'}
+    )
+
+    def execute(self, context):
+        from . import dsts_formats as f
+        from .skeleton import export_skeleton
+        from .mesh import export_mesh_object
+
+        g = f.Geom()
+        g.skeleton = export_skeleton([o for o in bpy.data.objects if o.type == "ARMATURE"][0])
+        g.materials.append(f.Material())
+
+        for obj in bpy.data.objects:
+            if obj.type == "MESH":
+                mesh = export_mesh_object(obj, g.skeleton)
+                mesh.matrix_palette.append(g.skeleton.bones[0])
+                mesh.material = g.materials[0]
+                g.meshes.append(mesh)
+
+        g.to_file(self.filepath)
+
+        return {'FINISHED'}
 
 def menu_func_import(self, context):
     self.layout.operator(MY_OT_dsts_geom_import_operator.bl_idname, text="DSTS .geom import")
 
+def menu_func_export(self, context):
+    self.layout.operator(MY_OT_dsts_geom_export_operator.bl_idname, text="DSTS .geom export")
+
 def register():
     bpy.utils.register_class(MY_OT_dsts_geom_import_operator)
     bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
+    bpy.utils.register_class(MY_OT_dsts_geom_export_operator)
+    bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
     material_nodes.register()
 
 def unregister():
     bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
     bpy.utils.unregister_class(MY_OT_dsts_geom_import_operator)
+    bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
+    bpy.utils.unregister_class(MY_OT_dsts_geom_export_operator)
     material_nodes.unregister()
